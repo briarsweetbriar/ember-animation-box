5201dbf694194082b39a6fcbd3a244a730e4f13d
null null null <null@affinitygamescollective.org>
2016-10-26 09:47:36 -0700

    make velocity adapter less agressive when clearing

--- addon/eab-animation-adapters/velocity.js
+++ addon/eab-animation-adapters/velocity.js
@@ -5,14 +5,27 @@ const {
   get
 } = Ember;
 
+const { RSVP: { resolve } } = Ember;
+
 export default Ember.Object.extend({
   animate(element, effect, options) {
     let finalEffect = effect;
 
     if (get(options, 'clear')) {
-      finalEffect = assign({ translateX: 0, translateY: 0, translateZ: 0 }, effect);
+      if (element.style.transform.includes('translateX')) {
+        finalEffect = assign({ translateX: 0 }, finalEffect);
+      }
+      if (element.style.transform.includes('translateY')) {
+        finalEffect = assign({ translateY: 0 }, finalEffect);
+      }
+      if (element.style.transform.includes('translateZ')) {
+        finalEffect = assign({ translateZ: 0 }, finalEffect);
+      }
+
       element.removeAttribute('style');
     }
+
+    if (Object.keys(finalEffect).length === 0) { return resolve(); }
 
     return Ember.$.Velocity.animate(element, finalEffect, options);
   }
